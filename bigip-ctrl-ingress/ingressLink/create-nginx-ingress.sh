c9a8df16e78bfa44038ac0f314b7393e0e1e9659
carloshzoghbi <37075619+carloshzoghbi@users.noreply.github.com>
2022-06-17 22:35:41 -0500

    Update create-nginx-ingress.sh

--- bigip-ctrl-ingress/ingressLink/create-nginx-ingress.sh
+++ bigip-ctrl-ingress/ingressLink/create-nginx-ingress.sh
@@ -1,35 +1,33 @@
 #!/bin/sh
-git clone https://github.com/nginxinc/kubernetes-ingress/
+git clone https://github.com/carloshzoghbi/kubernetes-ingress
 cd kubernetes-ingress/deployments
-git checkout v1.11.1
-#Set up files
-cp ../examples/complete-example/cafe.yaml .
-cp ../examples/complete-example/cafe-secret.yaml .
-wget https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/nginx-ingress/config/cafe-ingress.yaml
-wget https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/nginx-ingress/config/nginx-config.yaml
-wget https://raw.githubusercontent.com/laul7klau/kubernetes-aws/main/bigip-ctrl-ingress/ingressLink/config/nodeport.yaml 
-#Install Ingress Controller per https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests
+git checkout v2.1.1
+git switch main
+cp ../examples/appprotect/cafe.yaml .
+cp ../examples/appprotect/cafe-secret.yaml .
+cp ../examples/appprotect/cafe-ingress.yaml .
+cp ../examples/appprotect/ap-logconf.yaml .
+cp ../examples/appprotect/ap-dataguard-alarm-policy.yaml .
+cp ../examples/appprotect/ap-apple-uds.yaml .
 kubectl apply -f common/ns-and-sa.yaml
 kubectl apply -f rbac/rbac.yaml
-kubectl apply -f nginx-config.yaml
+kubectl apply -f rbac/ap-rbac.yaml
 kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
 kubectl apply -f common/ingress-class.yaml
-
 kubectl apply -f common/crds/k8s.nginx.org_virtualservers.yaml
 kubectl apply -f common/crds/k8s.nginx.org_virtualserverroutes.yaml
 kubectl apply -f common/crds/k8s.nginx.org_transportservers.yaml
 kubectl apply -f common/crds/k8s.nginx.org_policies.yaml
 kubectl apply -f common/crds/k8s.nginx.org_globalconfigurations.yaml
-kubectl apply -f common/global-configuration.yaml
-
-#Install Ingress Controller pods as daemonset
-kubectl apply -f daemon-set/nginx-ingress.yaml
-
-#Exposing service as nodeport and creating AWS loadbalancer
-kubectl create -f nodeport.yaml
-
-#Configuring coffee tea app by configuring Ingress Controller through ingress resource
+kubectl apply -f common/crds/appprotect.f5.com_aplogconfs.yaml
+kubectl apply -f common/crds/appprotect.f5.com_appolicies.yaml
+kubectl apply -f common/crds/appprotect.f5.com_apusersigs.yaml
+kubectl apply -f daemon-set/nginx-plus-ingress.yaml
+kubectl apply -f service/loadbalancer.yaml
 kubectl create -f cafe.yaml
 kubectl create -f cafe-secret.yaml
+kubectl create -f ap-logconf.yaml
+kubectl create -f ap-dataguard-alarm-policy.yaml
+kubectl create -f ap-apple-uds.yaml
 kubectl create -f cafe-ingress.yaml
-cd ../..
